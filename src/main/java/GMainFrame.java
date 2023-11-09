7840d9f5615a8699e02dbf00fb6af1831b111bee
ATAI <nyhryan99102@gmail.com>
2023-11-09 16:06:54 +0900

    프로그램 테마(LaF)를 Nimbus로 바꾸고, 화면 크기도 줄였음.

--- src/main/java/GMainFrame.java
+++ src/main/java/GMainFrame.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.UIManager.*;
 
 /**
  * GUI 프로그램의 메인 프레임
@@ -9,8 +10,19 @@ public class GMainFrame extends JFrame {
         setTitle("그래프 프로그램");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(400, 300));
-        setSize(new Dimension(1280, 960));
+        setSize(new Dimension(800, 600));
         setLayout(new CardLayout());
+
+        try {
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         // 현재 메인 프레임에 서브 패널들을 추가한다.

0ad09027bf4dbaf4c6ad50449fb0a0aaea01fc8b
nyhryan <nyhryan99102@gmail.com>
2023-11-12 00:20:04 +0900

    그래프를 표시하는 패널과 정보를 표시하는 패널을 GVisualPanelWrapper패널로 감싸음.

--- src/main/java/GMainFrame.java
+++ src/main/java/GMainFrame.java
@@ -11,18 +11,14 @@ public class GMainFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(400, 300));
         setSize(new Dimension(800, 600));
-        setLayout(new CardLayout());
 
         setLocationRelativeTo(null); // 화면 중앙에 프로그램을 배치
 
         // 현재 메인 프레임에 서브 패널들을 추가한다.
-        add("Algorithm.Graph Mode", gVisualPanel);
-        add("Quiz Mode", gQuizPanel);
-
+        setLayout(new CardLayout());
+        add("Graph Mode", new GVisualPanelWrapper());
+//        add("Quiz Mode", gQuizPanel);
     }
 
-    // 그래프를 그리는 서브 패널
-    private GVisualPanel gVisualPanel = new GVisualPanel();
-
-    private GQuizPanel gQuizPanel = new GQuizPanel();
+   private GQuizPanel gQuizPanel = new GQuizPanel();
 }
---
+++ src/main/java/GVisualPanelWrapper.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+
+class GVisualPanelWrapper extends JPanel {
+    public GVisualPanelWrapper() {
+        setLayout(new BorderLayout());
+
+        // 화면 상단에 툴바를 추가한다.
+        JToolBar toolBar = new GVisualPanelToolBar(gVisualPanel);
+        add(toolBar, BorderLayout.NORTH);
+
+        // 그래프를 그리는 스크롤 패널
+        JScrollPane scrollPane = new JScrollPane(gVisualPanel);
+        gVisualPanel.setPreferredSize(new Dimension(2000, 2000));
+
+        JScrollPane scrollPane2 = new JScrollPane(gInfoPanel);
+
+        // 스플릿 패널에 그래프와 그래프 정보를 그리는 패널을 추가한다.
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, scrollPane2);
+        splitPane.setDividerLocation(0.5);
+        splitPane.setResizeWeight(0.7);
+        splitPane.setOneTouchExpandable(true);
+
+        // splitPane의 divider가 움직이면 발생하는 이벤트 리스너 추가
+        splitPane.addPropertyChangeListener(JSplitPane.DIVIDER_LOCATION_PROPERTY, e -> {
+            gVisualPanel.repaint();
+            gInfoPanel.repaint();
+        });
+
+        // 스플릿 패널을 현재 패널에 추가한다.
+        add(splitPane, BorderLayout.CENTER);
+
+        // 화면을 더블클릭한 위치에 새로운 노드 추가
+        gVisualPanel.addMouseListener(new GVisualPanelMouseAdapter(this));
+        gVisualPanel.addMouseMotionListener(new GVisualPanelMouseAdapter(this));
+
+    }
+
+    // 그래프를 그리는 패널
+    private GVisualPanel gVisualPanel = new GVisualPanel();
+
+    public GInfoPanel getgInfoPanel() {
+        return gInfoPanel;
+    }
+
+    private GInfoPanel gInfoPanel = new GInfoPanel(gVisualPanel.getGraph());
+
+    public GVisualPanel getgVisualPanel() {
+        return gVisualPanel;
+    }
+}

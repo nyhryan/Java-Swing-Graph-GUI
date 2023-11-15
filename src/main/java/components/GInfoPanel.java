f6cc0f24ce32d36f38061f93f0847d9585ebbdbc
nyhryan <nyhryan99102@gmail.com>
2023-11-16 02:23:22 +0900

    알고리즘 애니메이션 구현에 앞서 임시로 노드만 순회하는 알고리즘을 스레딩으로 구현해봄.

--- src/main/java/components/GInfoPanel.java
+++ src/main/java/components/GInfoPanel.java
@@ -25,25 +25,38 @@ class GInfoPanel extends JPanel {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        // 그래프 정보를 출력할 창에 CSS를 적용한다.
         HTMLEditorKit kit = new HTMLEditorKit();
         kit.getStyleSheet().addRule("span, p { margin: 0; padding: 0; font-size: 14px; } li { font-size: 14px; }");
 
+        // 그래프 정보를 출력할 JEditorPane
         editorPane = new JEditorPane();
-
         editorPane.setEditable(false);
         editorPane.setEditorKit(kit);
         editorPane.setBackground(Color.WHITE);
 
+        // 그래프 정보를 출력할 JEditorPane을 스크롤 패널에 추가한다.
         JScrollPane scrollPane = new JScrollPane(editorPane);
         scrollPane.setPreferredSize(new Dimension(300, 300));
         add(scrollPane);
 
+        // 알고리즘 테스트 버튼
         JPanel buttonPanel = new JPanel();
-        buttonPanel.add(new JButton("1"));
+        JButton testBtn = new JButton("Test");
+
+        testBtn.addActionListener(e -> {
+            // 버튼을 누르면 알고리즘 테스트를 위한 쓰레드 생성 및 시작
+            SwingUtilities.invokeLater(() -> {
+                Thread t = new Thread(new TestAlgorithm(gVisualPanelWrapper));
+                t.start();
+            });
+        });
+        
+        // 알고리즘 버튼을 임시로 추가
+        buttonPanel.add(testBtn);
         buttonPanel.add(new JButton("2"));
         buttonPanel.add(new JButton("3"));
         buttonPanel.add(new JButton("4"));
-
 
         add(buttonPanel);
     }

ed47d354e0ffe81ec665fdde34f429ba6e14fa5d
nyhryan <nyhryan99102@gmail.com>
2023-12-10 23:18:43 +0900

    ImageIconLodaer.getImageIcon() 리팩터링

--- src/main/java/components/ImageIconLoader.java
+++ src/main/java/components/ImageIconLoader.java
@@ -6,10 +6,10 @@ import java.util.Objects;
 
 public class ImageIconLoader {
     public static ImageIcon getImageIcon(String path, int w) {
-        ImageIcon stopIcon = new ImageIcon(Objects.requireNonNull(ImageIconLoader.class.getResource(path)));
-        Image image = stopIcon.getImage();
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(ImageIconLoader.class.getResource(path)));
+        Image image = icon.getImage();
         image = image.getScaledInstance(w, w, Image.SCALE_SMOOTH);
-        stopIcon.setImage(image);
-        return stopIcon;
+        icon.setImage(image);
+        return icon;
     }
 }

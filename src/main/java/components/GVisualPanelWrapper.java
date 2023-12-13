d2e5d02106a698e7a123b88c76701b9cbf281e25
nyhryan <nyhryan99102@gmail.com>
2023-12-13 15:49:44 +0900

    GVisualPanelWrapper 리팩토링

--- src/main/java/components/GVisualPanelWrapper.java
+++ src/main/java/components/GVisualPanelWrapper.java
@@ -6,16 +6,20 @@ import java.util.concurrent.Semaphore;
 
 public class GVisualPanelWrapper extends JPanel {
     public GVisualPanelWrapper() {
+        setLayout(new BorderLayout());
+
         // 이 둘 순서 바뀌면 프로그램이 정상 작동하지 않음. 주의!
         // gInfoPanel은 gVisualPanel의 정보에 의존하기 떄문에 순서를 지켜야한다.
         gVisualPanel = new GVisualPanel(this);
         gInfoPanel = new GInfoPanel(this);
 
-        setLayout(new BorderLayout());
+        // gVisualPanel에 마우스 이벤트 리스너 추가
+        gVisualPanel.addMouseListener(new GVisualPanelMouseAdapter(this));
+        gVisualPanel.addMouseMotionListener(new GVisualPanelMouseAdapter(this));
 
         // 화면 상단에 툴바를 추가한다.
-        JToolBar toolBar = new GVisualPanelToolBar(gVisualPanel);
-        add(toolBar, BorderLayout.NORTH);
+        GVisualPanelToolBar gVisualPanelToolBar = new GVisualPanelToolBar(this);
+        add(gVisualPanelToolBar, BorderLayout.NORTH);
 
         // 그래프를 그리는 스크롤 패널
         JScrollPane scrollPane = new JScrollPane(gVisualPanel);
@@ -35,10 +39,6 @@ public class GVisualPanelWrapper extends JPanel {
 
         // 스플릿 패널을 현재 패널에 추가한다.
         add(splitPane, BorderLayout.CENTER);
-
-        // 화면을 더블클릭한 위치에 새로운 노드 추가
-        gVisualPanel.addMouseListener(new GVisualPanelMouseAdapter(this));
-        gVisualPanel.addMouseMotionListener(new GVisualPanelMouseAdapter(this));
     }
 
     public GVisualPanel getgVisualPanel() {

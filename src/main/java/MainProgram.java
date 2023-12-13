2cb37a85bc8e7dc3e0628891f69eeea8c26b5b80
nyhryan <nyhryan99102@gmail.com>
2023-12-13 15:48:41 +0900

    MainProgram 리팩토링

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -3,20 +3,27 @@ import components.GMainFrame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.InputStream;
 
 public class MainProgram {
     public static void main(String[] args) {
         // 스윙 전용 스레드(EDT)에서 그래프 프로그램을 시작
         SwingUtilities.invokeLater(() -> {
             try {
+                // UI 테마 설정
                 UIManager.setLookAndFeel(new FlatCyanLightIJTheme());
-                var is = MainProgram.class.getResourceAsStream("/NotoSansKR-Regular.ttf");
-                assert is != null;
-                Font notoSansKR = Font.createFont(Font.TRUETYPE_FONT, is);
-                UIManager.put("defaultFont", notoSansKR.deriveFont(12f));
+
+                // UI 폰트 설정
+                InputStream is = MainProgram.class.getResourceAsStream("/NotoSansKR-Regular.ttf");
+                if (is != null) {
+                    Font notoSansKR = Font.createFont(Font.TRUETYPE_FONT, is);
+                    UIManager.put("defaultFont", notoSansKR.deriveFont(12f));
+                }
             } catch (Exception e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
+
+            // 메인 프레임 생성
             new GMainFrame().setVisible(true);
         });
     }

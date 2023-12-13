70df06c9ef7ee15d471e06f1c1ca446ba485939b
duddn0406 <64262606+duddn0406@users.noreply.github.com>
2023-12-13 23:42:08 +0900

    changed
    make question, change photo, change  check answer

--- src/main/java/components/GQuizPanel.java
+++ src/main/java/components/GQuizPanel.java
@@ -18,6 +18,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
+
 public class GQuizPanel extends JPanel {
     private int score = 0; // 사용자의 점수를 기록합니다.
     public class QuizData {
@@ -62,14 +63,13 @@ public class GQuizPanel extends JPanel {
 
         // 퀴즈 데이터를 생성합니다.
         List<QuizData> quizList = Arrays.asList(
-                new QuizData("/Quiz/Q1.png", "정답1", "문제1: 이것은 첫 번째 문제입니다.", "선택지1-1", "선택지1-2", "선택지1-3", "선택지1-4"),
-                new QuizData("/Quiz/Q2.png", "정답2", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4"),
-                new QuizData("/Quiz/Q3.png", "정답3", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4"),
-                new QuizData("/Quiz/Q4.png", "정답4", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4"),
-                new QuizData("/Quiz/Q5.png", "정답5", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4"),
-                new QuizData("/Quiz/Q6.png", "정답6", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4"),
-                new QuizData("/Quiz/Q7.png", "정답7", "문제2: 이것은 두 번째 문제입니다.", "선택지2-1", "선택지2-2", "선택지2-3", "선택지2-4")
-                // ... 나머지 퀴즈들
+        		new QuizData("Quiz/Q1.png", "B - 36", "나그네가 우물로 물을 뜨러 가려고 합니다. 화살표가 표시된 우물까지 우물과 우물을 지나서 가려고 합니다. 그때 총 거리의 합과 지나지 않는 우물이 바르게 짝지어 진것은?", "A - 45", "C - 36", "B - 37", "B - 36"),
+                new QuizData("Quiz/Q2.png", "4 - 36", "언제나 위험한 바닷속에 사는 물고기는 언제나 도망갈 길을 만들어 둔다. 물고기가 위험에 처했을때 도망갈 곳이 별표가 있는 산호일때 물고기는 최단거리로 간다고 할때 지나는 산호의 갯수와 거리가 바르게 짝지어 진것은?", "4 - 36", "5 - 29", "3 - 34", "2 - 38"),
+                new QuizData("Quiz/Q3.png", "24", "방이 어두워 전등을 켜려고 한다. 스위치를 누르면 별표가 있는 전등까지 최단거리로 켜진다고 할때 지나는 전선의 길이는 얼마인가?", "25", "24", "27", "29"),
+                new QuizData("Quiz/Q4.png", "17", "강아지가 공놀이 중이다. 이강아지는 공을 가져오기 위해 최소비용 신장트리를 따라 움직인다고 한다. 그렇다면 강아지가 제자리에서 'E' 공을 가져오기 위해 가야하는 거리는 얼마인가?", "15", "16", "17", "18"),
+                new QuizData("Quiz/Q5.png", "G", "철수는 길을 잃었다. 기억나는 것을 각 아파트와 학교를 이은 그래프를 Kruskal알고리즘에 따라 최소비용 신장 트리를 만들다 보면 학교 다음으로 선택된 아파트라고 할때 철수의 집은 어디인가?", "A", "B", "E", "G"),
+                new QuizData("Quiz/Q6.png", "A-C-G-D-E-F-B-H", "하늘의 별을 보고 prim알고리즘에 따라 별자리를 만들려고 할때 선택된 별의 순서로 알맞은 것은?", "A-B-C-D-E-F-G-H", "A-C-G-D-E-F-B-H", "G-D-A-B-E-C-F-H", "H-B-F-E-D-G-C-A"),
+                new QuizData("Quiz/Q7.png", "3", "이 배는 prim알고리즘에 따라 부표로 향해 작업을 한다. 이때 표시된 부표까지 몇개의 작업을 시행하며 넘어오는가?", "1", "2", "3", "4")
         );
         Collections.shuffle(quizList);
         quizIterator = quizList.iterator();
@@ -92,7 +92,7 @@ public class GQuizPanel extends JPanel {
         problemDescriptionArea.setEditable(false);
         problemDescriptionArea.setLineWrap(true);
         problemDescriptionArea.setWrapStyleWord(true);
-        problemDescriptionArea.setFont(new Font("SansSerif", Font.PLAIN, 14));
+        problemDescriptionArea.setFont(new Font("SansSerif", Font.PLAIN, 30));
         bottomPanel.add(problemDescriptionArea);
         for (String option : quiz.getOptions()) {
             JCheckBox checkBox = new JCheckBox(option);
@@ -104,6 +104,15 @@ public class GQuizPanel extends JPanel {
         submitBtn.setPreferredSize(new Dimension(120, 80));
         submitBtn.addActionListener(e -> {
             remove(panel);
+            for (Component component : bottomPanel.getComponents()) {
+                if (component instanceof JCheckBox) {
+                    JCheckBox checkBox = (JCheckBox) component;
+                    // 체크박스가 선택되었고, 선택한 옵션이 정답과 같은 경우에 점수를 증가시킵니다.
+                    if (checkBox.isSelected() && checkBox.getText().equals(quiz.getAnswer())) {
+                        score++; // 선택한 옵션이 정답인 경우 점수를 증가시킴
+                    }
+                }
+            }
 
             if (quizIterator.hasNext()) {
                 add(createQuizPanel(quizIterator.next()));
